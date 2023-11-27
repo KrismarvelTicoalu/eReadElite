b00fa49566c77dd97b0377a2c905a9956ca7c203
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2023-11-27 21:18:57 +0800

    configure routes of all genres in catalog

--- eReadElite/src/pages/Homepage/index.jsx
+++ eReadElite/src/pages/Homepage/index.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer"
 import Header from "../../components/Header"
 
 const HomePage = () => {
@@ -415,28 +416,7 @@ const HomePage = () => {
   </div>
   {/* cooming  section end */}
   {/* footer  section start */}
-  <div className="footer_section layout_padding">
-    <div className="container">
-      <div className="footer_menu">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/movies">Movies</a></li>
-          <li><a href="/tv">TV</a></li>
-          <li><a href="/celebs">Celebs</a></li>
-          <li><a href="#">Sports</a></li>
-          <li><a href="#">News</a></li>
-        </ul>
-      </div>
-      <div className="social_icon">
-        <ul>
-          <li><a href="#"><img src="images/fb-icon.png" /></a></li>
-          <li><a href="#"><img src="images/twitter-icon.png" /></a></li>
-          <li><a href="#"><img src="images/linkedin-icon.png" /></a></li>
-          <li><a href="#"><img src="images/instagram-icon.png" /></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  <Footer />
   {/* footer  section end */}
   {/* copyright section start */}
   <div className="copyright_section">
--- eReadElite/src/pages/Mystery/index.jsx
+++ eReadElite/src/pages/Mystery/index.jsx
@@ -1,7 +1,164 @@
+import Header from "../../components/Header"
+
 const Mystery = () => {
   return (
     <div>
-        <h1>Mystery</h1>
+        <div>
+  {/* header section start */}
+  <Header />
+  {/* header section end */}
+  {/* movies section start */}
+  <div className="movies_section layout_padding">
+    <div className="container">
+      <div className="movies_menu">
+        <ul>
+          <li><a href="/catalog">Overview</a></li>
+          <li className="active"><a href="/catalog/mystery">Mystery</a></li>
+          <li><a href="/catalog/sci-fi">Sci-fi</a></li>
+          <li><a href="/catalog/romance">Romance</a></li>
+          <li><a href="/catalog/educational">Educational</a></li>
+          <li><a href="/catalog/theology">Theology</a></li>
+        </ul>
+      </div>
+      <div className="movies_section_2 layout_padding">
+        <h2 className="letest_text">Mystery</h2>
+        <div className="seemore_bt"><a href="#">See More</a></div>
+        <div className="movies_main">
+          <div className="iamge_movies_main">
+            <div className="iamge_movies">
+              <div className="image_3">
+                <img src="../images/img-3.png" className="image" style={{width: '100%'}} />
+                <div className="middle">
+                  <div className="playnow_bt">Play Now</div>
+                </div>
+              </div>
+              <h1 className="code_text">CADE Prlor</h1>
+              <p className="there_text">There are many variations </p>
+              <div className="star_icon">
+                <ul>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="iamge_movies">
+              <div className="image_3">
+                <img src="../images/img-4.png" className="image" style={{width: '100%'}} />
+                <div className="middle">
+                  <div className="playnow_bt">Play Now</div>
+                </div>
+              </div>
+              <h1 className="code_text">Bradon</h1>
+              <p className="there_text">There are many variations </p>
+              <div className="star_icon">
+                <ul>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="iamge_movies">
+              <div className="image_3">
+                <img src="../images/img-5.png" className="image" style={{width: '100%'}} />
+                <div className="middle">
+                  <div className="playnow_bt">Play Now</div>
+                </div>
+              </div>
+              <h1 className="code_text">Anton Levin</h1>
+              <p className="there_text">There are many variations </p>
+              <div className="star_icon">
+                <ul>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="iamge_movies">
+              <div className="image_3">
+                <img src="../images/img-6.png" className="image" style={{width: '100%'}} />
+                <div className="middle">
+                  <div className="playnow_bt">Play Now</div>
+                </div>
+              </div>
+              <h1 className="code_text">Sacha Styles</h1>
+              <p className="there_text">There are many variations </p>
+              <div className="star_icon">
+                <ul>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="iamge_movies">
+              <div className="image_3">
+                <img src="../images/img-7.png" className="image" style={{width: '100%'}} />
+                <div className="middle">
+                  <div className="playnow_bt">Play Now</div>
+                </div>
+              </div>
+              <h1 className="code_text">Katledrazdu</h1>
+              <p className="there_text">There are many variations </p>
+              <div className="star_icon">
+                <ul>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                  <li><a href="#"><img src="../images/star-icon.png" /></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="seebt_1"><a href="#">See More</a></div>
+    </div>
+  </div>
+  {/* movies section end */}
+  {/* footer  section start */}
+  <div className="footer_section layout_padding">
+    <div className="container">
+      <div className="footer_menu">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="movies.html">Movies</a></li>
+          <li><a href="tv.html">TV</a></li>
+          <li><a href="celebs.html">Celebs</a></li>
+          <li><a href="#">Sports</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+      </div>
+      <div className="social_icon">
+        <ul>
+          <li><a href="#"><img src="images/fb-icon.png" /></a></li>
+          <li><a href="#"><img src="images/twitter-icon.png" /></a></li>
+          <li><a href="#"><img src="images/linkedin-icon.png" /></a></li>
+          <li><a href="#"><img src="images/instagram-icon.png" /></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  {/* footer  section end */}
+  {/* copyright section start */}
+  <div className="copyright_section">
+    <div className="container">
+      <div className="copyright_text">Copyright 2019 All Right Reserved By <a href="https://html.design">Free html Templates</a></div>
+    </div>
+  </div>
+</div>
+
     </div>
   )
 }
