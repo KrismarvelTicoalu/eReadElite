5ea388d0015a7d55f9cba5946986629eb8ae9f7c
chanliearyandousem <aryandochanlie@gmail.com>
2023-12-01 15:46:22 +0800

    menambahkan cover

--- eReadElite/src/pages/BookProfile/index.jsx
+++ eReadElite/src/pages/BookProfile/index.jsx
@@ -2,10 +2,15 @@ import Header from "../../components/Header";
 
 const BookProfile = () => {
     return (
-      <div>
+      <>
        <Header/>
-  
-      </div>
+       
+       <a href="#" className="Profilbuku">
+         <img src="images/mysteryBook.jpg" className="bukuMystery" alt="Mystery Book" />
+      </a>
+      
+    
+      </>
     )
   }
   
